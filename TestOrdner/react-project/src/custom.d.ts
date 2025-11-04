fcb1f86cf158232320e89bcb7399188ee00ce0fa
Maik Protze <maik.protze@dci-student.org>
2025-11-04 15:38:35 +0100

    Wintergarten mit onClick plus Vergrößerung beim anklicken

--- TestOrdner/react-project/src/custom.d.ts
+++ TestOrdner/react-project/src/custom.d.ts
@@ -1,3 +1,7 @@
+/*
+ Typedeklaration für TypScript 
+ da es den zusätzlichen ReactComponent aus SVG-Datei nicht kennt*/
+
 declare module '*.svg' {
   import * as React from 'react'
   export const ReactComponent: React.FunctionComponent<
